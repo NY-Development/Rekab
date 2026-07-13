27ed0a7d523afcc495ae4bd393ef2c363c28a0e7
Yamlak Negash <yamlaknegash96@gmail.com>
2026-07-13 13:09:50 +0300

    feat : appropriate info message added.

--- nydl-web/src/features/courses/pages/CourseCatalogPage.tsx
+++ nydl-web/src/features/courses/pages/CourseCatalogPage.tsx
@@ -103,9 +103,12 @@ export default function CourseCatalogPage() {
                   {course.description}
                 </p>
                 <div className="flex justify-between items-center mt-auto border-t border-slate-100 pt-4" onClick={(e) => e.stopPropagation()}>
-                  <span className="text-lg font-semibold text-slate-900">ETB{course.price || '999'}</span>
+                  <span className="text-lg font-semibold text-slate-900">ETB {course.price || '999'}</span>
                   <button
-                    onClick={() => navigate(`/enroll/${course.id}`)}
+                    onClick={() => {
+                      window.location.href='https://nydev-form-generation.vercel.app/f/nydev-learning-nydl-summer-cohort-registration-2026'
+                      // navigate(`/enroll/${course.id}`)
+                    }}
                     className="bg-blue-600 hover:bg-blue-700 text-white text-xs font-semibold px-4 py-2 rounded-md transition-colors shadow-sm"
                   >
                     Enroll Now
